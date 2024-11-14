92b8c28afde23aace50caef7e1caa24a9191a387
Andrew Reddin <areddin409@gmail.com>
2024-11-14 17:47:01 -0600

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import {Button} from "@/components/ui/button.tsx";
+
+function App() {
+
+  return (
+  <Button variant={'secondary'} size={'lg'} className={'text-blue-500'}>Click Me</Button>
+  )
+}
+
+export default App
