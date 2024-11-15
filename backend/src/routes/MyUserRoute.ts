59abbe4868fbf31064f37dce75f5160545840b42
Andrew Reddin <areddin409@gmail.com>
2024-11-15 10:39:39 -0600

    Initial Auth Complete

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,15 +1,20 @@
 // MyUserRoute.ts
 import express, { NextFunction, Request, Response } from "express";
 import MyUserController from "../controllers/MyUserController";
+import { jwtCheck } from "../middleware/auth";
 
 const router = express.Router();
 
-router.post("/", async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    await MyUserController.createCurrentUser(req, res);
-  } catch (error) {
-    next(error);
-  }
-});
+router.post(
+  "/",
+  jwtCheck,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await MyUserController.createCurrentUser(req, res);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
 
 export default router;
--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -8,11 +8,13 @@ type Props = {
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
   const navigate = useNavigate();
+
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-  if (!domain || !clientId || !redirectUri) {
+  if (!domain || !clientId || !redirectUri || !audience) {
     throw new Error('Unable to retrieve Auth0 configuration');
   }
 
@@ -24,7 +26,10 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      authorizationParams={{ redirect_uri: redirectUri }}
+      authorizationParams={{
+        redirect_uri: redirectUri,
+        audience,
+      }}
       onRedirectCallback={onRedirectCallback}
     >
       {children}
