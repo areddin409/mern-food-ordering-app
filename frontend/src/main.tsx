4dcaeb54db5d7ca9fb3f7d1678bb6ef1e24b4aea
Andrew Reddin <areddin409@gmail.com>
2024-11-14 20:53:38 -0600

    Adding Auth0

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {BrowserRouter as Router}  from "react-router-dom";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import './global.css'
-import AppRoutes from "@/AppRoutes.tsx";
+import './global.css';
+import AppRoutes from '@/AppRoutes.tsx';
+import Auth0ProviderWithNavigate from '@/auth/Auth0ProviderWithNavigate.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <Router>
-        <AppRoutes/>
-      </Router>
-
-  </StrictMode>,
-)
+    <Router>
+      <Auth0ProviderWithNavigate>
+        <AppRoutes />
+      </Auth0ProviderWithNavigate>
+    </Router>
+  </StrictMode>
+);
