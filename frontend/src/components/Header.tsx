636e51a176463ec37e4a08e7be3a0da2d4c73288
Andrew Reddin <areddin409@gmail.com>
2024-11-14 18:56:51 -0600

    working on header

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className={'border-b-2 border-b-orange-500 py-6'}>
+        <div className="container mx-auto flex justify-between items-center"></div>
+    </div>
+  );
+}
